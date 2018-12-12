9599e0057ec75b0ed896cd616eafbbad41a22abf
Daniela Di Iorio <danielad@gmail.com>
2018-12-11 16:21:12 -0800

    mapped final poem to show line by line

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -8,7 +8,7 @@ const FinalPoem = (props) => {
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-        <p>{props.poemLines}</p>
+        {props.poemLines.map((line) => <p>{line}</p>)}
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
